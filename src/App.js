cebcd34194e217b72bd688d3a917da669c6388f9
maxiu271299 <maksymilian.kierski@gmail.com>
2020-03-06 00:48:11 +0100

    Get all needed elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import { ReactComponent as Idea } from "./idea.svg";
+import gsap from 'gsap';
 
 function App() {
-  return (
-    <div className="App">
+  const wrapper = useRef(null);
 
+  useEffect(() => {
+    const [elements] = wrapper.current.children;
+
+    const background = elements.getElementById("background");
+    const person = elements.getElementById("person");
+    const bulb = elements.getElementById("bulb");
+    const light = elements.getElementById("Vector_35");
+  });
+
+  return (
+    <div ref={wrapper} className="App">
+      <Idea />
     </div>
   );
 }
