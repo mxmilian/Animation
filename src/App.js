9a5e55ef40391e7935bf51d1b7528a9462deb9cb
maxiu271299 <maksymilian.kierski@gmail.com>
2020-03-06 01:10:59 +0100

    Finished

--- src/App.js
+++ src/App.js
@@ -13,7 +13,18 @@ function App() {
     const person = elements.getElementById("person");
     const bulb = elements.getElementById("bulb");
     const light = elements.getElementById("Vector_35");
-  });
+
+    gsap.set([...background.children, person, bulb, light], {autoAlpha: 0});
+    gsap.set(bulb, {transformOrigin: '58% 100%'});
+    gsap.set(light, {transformOrigin: '75% 45%'});
+
+    const timeLine = gsap.timeline({defaults: {ease: 'power3.inOut'}});
+
+    timeLine.fromTo(person, {x: '-=600'}, {duration:2, x: '+=600', autoAlpha: 1})
+        .fromTo(bulb, {scaleY: 0}, {duration: 0.3, autoAlpha: 1, scaleY:1})
+        .fromTo(light, {scale: .85}, {duration: 1, scale: 1, autoAlpha: 1}, '-=.25')
+        .to(background.children, {duration: 3, autoAlpha:1, stagger: 0.3})
+  }, []);
 
   return (
     <div ref={wrapper} className="App">
